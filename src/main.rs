d54159551be5b2ebf466c76984183fdac6ca29a6
Felipe Coury <felipe.coury@gmail.com>
2022-07-11 14:54:29 -0300

    feat: parses simple exemple successfully

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,123 @@
-use wasmedge_quickjs::*;
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
 
-fn main() {
-    let mut ctx = Context::new();
-    let code = r#"
-        import('peg-0.10.0.js').then((peg) => {
-            const parser = peg.generate(`
-                start = "a"
-            `);
-            return parser.parse("a");
-        });
-    "#;
+use pest::{error::Error, iterators::Pair, Parser};
 
-    let p = ctx.eval_global_str(code);
-    ctx.promise_loop_poll();
-    println!("after poll:{:?}", p);
-    if let JsValue::Promise(ref p) = p {
-        let v = p.get_result();
-        println!("v = {:?}", v);
-    }
+#[derive(Parser)]
+#[grammar = "mongodb.pest"]
+pub struct MongoDbParser;
+
+#[derive(Debug, Clone)]
+struct Expression {
+    clauses: Vec<LeafClause>,
 }
 
-// extern crate pest;
-// #[macro_use]
-// extern crate pest_derive;
+#[derive(Debug, Clone)]
+struct LeafClause {
+    pub key: String,
+    pub value: LeafValue,
+}
 
-// use pest::Parser;
+#[derive(Debug, Clone)]
+struct LeafValue {
+    value: String,
+}
 
-// #[derive(Parser)]
-// #[grammar = "mongodb.pest"]
-// pub struct MongoDbParser;
+fn main() {
+    let str = r#"{"status": "1"}"#;
+    let res = parse(str);
+    println!("{:?}", res);
+}
 
-// fn main() {
-//     let successful_parse =
-//         MongoDbParser::parse(Rule::json, r#"{"status":{"$in":["A","D"]},"x":2}"#);
-//     println!("{:?}", successful_parse);
-// }
+fn parse(query: &str) -> Result<Expression, Error<Rule>> {
+    let pair = MongoDbParser::parse(Rule::query, query)?.next().unwrap();
+
+    fn parse_query(query: Pair<Rule>) -> Expression {
+        let expression = query.into_inner().next().unwrap();
+
+        let clause_list = expression.into_inner().next().unwrap();
+        match clause_list.as_rule() {
+            Rule::clause_list => Expression {
+                clauses: parse_clause_list(clause_list),
+            },
+            _ => unreachable!(),
+        }
+    }
+
+    fn parse_clause_list(clause: Pair<Rule>) -> Vec<LeafClause> {
+        clause.into_inner().map(|pair| parse_clause(pair)).collect()
+    }
+
+    fn parse_clause(outer_clause: Pair<Rule>) -> LeafClause {
+        let clause = outer_clause.into_inner().next().unwrap();
+
+        match clause.as_rule() {
+            Rule::leaf_clause => {
+                let mut inner = clause.into_inner();
+                let key = inner.next().unwrap().as_str();
+                let value = parse_value(inner.next().unwrap());
+                LeafClause {
+                    key: key.to_string(),
+                    value,
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn parse_value(value: Pair<Rule>) -> LeafValue {
+        LeafValue {
+            value: value.as_str().to_string(),
+        }
+    }
+
+    Ok(parse_query(pair))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_json_object_null() {
+        let parse = MongoDbParser::parse(Rule::json, r#"{"status": null}"#);
+        assert!(parse.is_ok());
+    }
+
+    #[test]
+    fn test_object_null() {
+        let parse = MongoDbParser::parse(Rule::object, r#"{"status": null}"#);
+        assert!(parse.is_ok());
+    }
+
+    #[test]
+    fn test_object_string() {
+        let parse = MongoDbParser::parse(Rule::object, r#"{"status": "some"}"#);
+        assert!(parse.is_ok());
+    }
+
+    #[test]
+    fn test_member_null() {
+        let parse = MongoDbParser::parse(Rule::member, r#""status": null"#);
+        assert!(parse.is_ok());
+    }
+
+    #[test]
+    fn test_member_false() {
+        let parse = MongoDbParser::parse(Rule::member, r#""status": false"#);
+        assert!(parse.is_ok());
+    }
+
+    #[test]
+    fn test_member_true() {
+        let parse = MongoDbParser::parse(Rule::member, r#""status": true"#);
+        assert!(parse.is_ok());
+    }
+
+    #[test]
+    fn test_member_string() {
+        let parse = MongoDbParser::parse(Rule::member, r#""status": "true""#);
+        assert!(parse.is_ok());
+    }
+}
